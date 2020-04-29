43f8a09fb2304507562bb1c4f88e3915baf3459a
IlyaGAnyVision <ilyag@anyvision.co>
2020-04-29 19:04:07 +0300

    Updates

---
+++ precache-manifest.84cc0df6d782c5ab3dfd262a00e74af6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e92d2e813208f46de1eb5e26b0968688",
+    "url": "/react-master/index.html"
+  },
+  {
+    "revision": "6d9ad776bc4c0ee436ba",
+    "url": "/react-master/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b4d486582e7004593fc6",
+    "url": "/react-master/static/js/2.d8901f3e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-master/static/js/2.d8901f3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d9ad776bc4c0ee436ba",
+    "url": "/react-master/static/js/main.7be1a871.chunk.js"
+  },
+  {
+    "revision": "67aca1fa02e84028b43e",
+    "url": "/react-master/static/js/runtime-main.4caf2467.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-master/static/media/logo.5d5d9eef.svg"
+  }
+]);
